e2ec083c71c2668bd62e95126431730d0d22eedb
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2023-02-18 00:13:54 +0100

    Reset

--- src/structure/Miner.cs
+++ src/structure/Miner.cs
@@ -6,15 +6,8 @@ using SFML.System;
 public class Miner : Structure
 {
 
-    private int myVariable;
-
-    public int MyVariable
-    {
-        get { return myVariable; }
-        set { myVariable = value; }
-    }
-
     public int oreCount = 1;
+    
 
     public Miner()
     : base(ResourceLoader.fetchTexture(ResourceLoader.TextureType.Bed), new Vector2i(2, 1))
@@ -25,12 +18,12 @@ public class Miner : Structure
     public override void highlight()
     {
         base.highlight();
-        infoMenu.addItem(new TextButton(oreCount, () => {}));
+        infoMenu.addItem(new TextButton("oreCount", () => {}));
     }
 
     public override void update()
     {
         base.update();
-        oreCount.value ++;
+        //oreCount.value ++;
     }
 }
