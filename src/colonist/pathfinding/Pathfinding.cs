05fa7b804a22c7d75d33d412852cc2093ad57aa6
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2023-02-21 22:22:51 +0100

    A* pathfinding

--- src/colonist/pathfinding/Pathfinding.cs
+++ src/colonist/pathfinding/Pathfinding.cs
@@ -7,78 +7,90 @@ using System.Numerics;
 
 public class Pathfinding
 {
-    
-    public List<Tile> getPathTiles(Tile startTile, Tile endTile)
+    public List<Tile> findPath(Tile startTile, Tile endTile)
     {
-        PriorityQueue<Tile, float> discoveredTiles = new PriorityQueue<Tile, float>();
-        discoveredTiles.Enqueue(startTile, 0f); 
-        List<Tile> closedTiles = new List<Tile>();
+        var openSet = new List<Tile>() { startTile };
+        var cameFrom = new Dictionary<Tile, Tile>();
+        var gScore = new Dictionary<Tile, float>() { { startTile, 0 } };
+        var fScore = new Dictionary<Tile, float>() { { startTile, Heuristic(startTile, endTile) } };
 
-        Dictionary<Tile, float> costs = new Dictionary<Tile, float>{
-            {startTile, 0f}
-        };
-
-        while (discoveredTiles.Count > 0)
+        while (openSet.Count > 0)
         {
-            Tile currentTile = discoveredTiles.Dequeue();
-            Console.WriteLine($"Checking current tile {currentTile}");
-            foreach(var i in closedTiles) Console.WriteLine(i);
+            var currentTile = openSet.OrderBy(t => fScore.ContainsKey(t) ? fScore[t] : float.MaxValue).First();
+
             if (currentTile == endTile)
             {
-                constructPath();
-                Log.Message("hejsan");
-                return new List<Tile>();
+                return ReconstructPath(cameFrom, currentTile);
             }
-            List<Tile> neighborTiles = getNeighborTiles(currentTile);
-            foreach (Tile neighborTile in neighborTiles)
+
+            openSet.Remove(currentTile);
+
+            foreach (var neighborTile in GetNeighborTiles(currentTile))
             {
-                if (!closedTiles.Contains(neighborTile))
+                var tentativeGScore = gScore[currentTile] + DistanceCost(currentTile, neighborTile);
+
+                if (!gScore.ContainsKey(neighborTile) || tentativeGScore < gScore[neighborTile])
                 {
-                    float distance = distanceCost(neighborTile, endTile) + distanceCost(currentTile, neighborTile);
-                    discoveredTiles.Enqueue(neighborTile, distance);
+                    cameFrom[neighborTile] = currentTile;
+                    gScore[neighborTile] = tentativeGScore;
+                    fScore[neighborTile] = tentativeGScore + Heuristic(neighborTile, endTile);
+
+                    if (!openSet.Contains(neighborTile))
+                    {
+                        openSet.Add(neighborTile);
+                    }
                 }
             }
-            closedTiles.Add(currentTile);
         }
         return new List<Tile>();
     }
 
-    public float distanceCost(Tile firstTile, Tile secondTile)
-    {
-        Vector2 distance;
-        Tuple<int, int> firstTileIndex = Map.Instance.getTileIndex(firstTile);
-        Tuple<int, int> secondTileIndex = Map.Instance.getTileIndex(secondTile);
-        distance = 
-            new Vector2(
-            firstTileIndex.Item1, 
-            firstTileIndex.Item2)
-            - 
-            new Vector2(
-                secondTileIndex.Item1, 
-                secondTileIndex.Item2);
-        return Math.Abs(distance.Length());
-    }
-
-    public void constructPath()
-    {
-
-    }
-
-    public List<Tile> getNeighborTiles(Tile startTile)
+    private List<Tile> GetNeighborTiles(Tile startTile)
     {
         List<Tile> neighborTiles = new List<Tile>();
         Tuple<int, int> tileIndex = Map.Instance.getTileIndex(startTile);
-        for (int x = tileIndex.Item1-1; x < tileIndex.Item1+1; x++)
+        for (int x = tileIndex.Item1 - 1; x <= tileIndex.Item1 + 1; x++)
         {
-            for (int y = tileIndex.Item2-1; y < tileIndex.Item2+1; y++)
+            for (int y = tileIndex.Item2 - 1; y <= tileIndex.Item2 + 1; y++)
             {
-                if (x < 0 || x > Map.Instance.tiles.GetLength(1) || y < 0 || y > Map.Instance.tiles.GetLength(1))
+                if (x < 0 || x >= Map.Instance.tiles.GetLength(0) || y < 0 || y >= Map.Instance.tiles.GetLength(1))
                 {
                     continue;
                 }
                 if (Map.Instance.tiles[x, y] != startTile) neighborTiles.Add(Map.Instance.tiles[x, y]);
-            }   
+            }
         }
         return neighborTiles;
+    }
+
+    private float DistanceCost(Tile firstTile, Tile secondTile)
+    {
+        Vector2 distance;
+        Tuple<int, int> firstTileIndex = Map.Instance.getTileIndex(firstTile);
+        Tuple<int, int> secondTileIndex = Map.Instance.getTileIndex(secondTile);
+        distance = new Vector2(
+            firstTileIndex.Item1,
+            firstTileIndex.Item2)
+            -
+            new Vector2(
+                secondTileIndex.Item1,
+                secondTileIndex.Item2);
+        return Math.Abs(distance.Length());
+    }
+
+    private float Heuristic(Tile startTile, Tile endTile)
+    {
+        return DistanceCost(startTile, endTile);
+    }
+
+    private List<Tile> ReconstructPath(Dictionary<Tile, Tile> cameFrom, Tile currentTile)
+    {
+        var totalPath = new List<Tile>() { currentTile };
+        while (cameFrom.ContainsKey(currentTile))
+        {
+            currentTile = cameFrom[currentTile];
+            totalPath.Insert(0, currentTile);
+        }
+        return totalPath;
     }
 }
