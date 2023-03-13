1a2a5482ff128aaf17777ad639cf9ccfc8e9917b
nozzelozze <92754883+nozzelozze@users.noreply.github.com>
2023-03-13 10:57:27 +0100

    Update ConveyorBelt.cs

--- src/structure/buildings/logistics/ConveyorBelt.cs
+++ src/structure/buildings/logistics/ConveyorBelt.cs
@@ -1,7 +1,13 @@
 using System;
 using SFML.System;
 
-//public class ConveyorBelt : Structure
-//{
-//    
-//}
+public class ConveyorBelt : Structure
+{
+    
+    public ConveyorBelt()
+    : base ("Conveyor Belt", ResourceLoader.fetchTexture(ResourceLoader.TextureType.Copper), new Vector2i(1, 1), new Dictionary<Item.Type, int>())
+    {
+
+    }
+
+}
